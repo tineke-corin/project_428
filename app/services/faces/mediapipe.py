45b9f178876b1e7de0c7c1addaa23b35f1fec88a
Tineke Corin <61185778+tineke-corin@users.noreply.github.com>
2026-05-02 17:47:45 +1200

    Move mediapipe model init (#6)
    * Add get_model() function to the mediapipe detector so it
    does not load unless required.
    
    * Make directory command line arg required

--- app/services/faces/mediapipe.py
+++ app/services/faces/mediapipe.py
@@ -13,24 +13,30 @@ from mediapipe.tasks.python.vision import drawing_styles
 
 from app.services.utils import Point, BoundingBox
 
-# this is the best bet from MP, but is still only decent for faces within 5 metres of the camera
-# for faces further away you would need to implement tiling
-mp_fd_model_file = 'models/face_detection_full_range.tflite'
+_mediapipe_model = None
 
-if not os.path.isfile(mp_fd_model_file):
-    os.makedirs('models/', exist_ok=True)
-    wget.download('https://storage.googleapis.com/mediapipe-assets/face_detection_full_range.tflite', out='models/'),
+def get_model():
+  global _mediapipe_model
+  if _mediapipe_model is None:
+    # this is the best bet from MP, but is still only decent for faces within 5 metres of the camera
+    # for faces further away you would need to implement tiling
+    mp_fd_model_file = 'models/face_detection_full_range.tflite'
 
-# MediaPipe detector
-# Create FaceDetector object.
-face_base_options = python.BaseOptions(model_asset_path=mp_fd_model_file)
-face_options = vision.FaceDetectorOptions(base_options=face_base_options, running_mode=vision.RunningMode.IMAGE, min_detection_confidence=0.2)
-mp_face_detector = vision.FaceDetector.create_from_options(face_options)
+    if not os.path.isfile(mp_fd_model_file):
+      os.makedirs('models/', exist_ok=True)
+      wget.download('https://storage.googleapis.com/mediapipe-assets/face_detection_full_range.tflite', out='models/'),
+
+    # MediaPipe detector
+    # Create FaceDetector object.
+    face_base_options = python.BaseOptions(model_asset_path=mp_fd_model_file)
+    face_options = vision.FaceDetectorOptions(base_options=face_base_options, running_mode=vision.RunningMode.IMAGE, min_detection_confidence=0.2)
+    _mediapipe_model = vision.FaceDetector.create_from_options(face_options)
 
 def detect_faces_mediapipe(image_path):
+  model = get_model()
   cv_mat = cv2.imread(image_path)
   image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv_mat)
-  detection_result = mp_face_detector.detect(image)
+  detection_result = model.detect(image)
 
   detections = []
   for det in detection_result.detections:
